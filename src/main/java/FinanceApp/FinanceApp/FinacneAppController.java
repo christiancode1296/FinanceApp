2317b8c9fe6bd60951df4e4bd83df4aef0758809
RamiEter <ramieter.a@gmail.com>
2025-10-14 20:08:12 +0200

    1.M ready

--- src/main/java/FinanceApp/FinanceApp/FinacneAppController.java
+++ src/main/java/FinanceApp/FinanceApp/FinacneAppController.java
@@ -3,11 +3,14 @@ package FinanceApp.FinanceApp;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class FinacneAppController {
 
     @GetMapping("/financeapp")
-    public String hello() {
-        return "Hello, FinanceApp! Welcome to our new application.";
+    public List<User> getUsers() {
+        return List.of(new User("user1", "password1"), new User("user2", "password2"));
     }
 }
