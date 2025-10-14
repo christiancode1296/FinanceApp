c4547cc1f08dabe2b82f9c463ffe248aa7b74784
RamiEter <ramieter.a@gmail.com>
2025-10-14 22:53:51 +0200

    project rebuilded and api is conected over frontend

--- backend/src/main/java/FinanceApp/FinanceApp/FinacneAppController.java
+++ backend/src/main/java/FinanceApp/FinanceApp/FinacneAppController.java
@@ -1,5 +1,6 @@
 package FinanceApp.FinanceApp;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,9 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/financeapp")
 public class FinacneAppController {
 
-    @GetMapping("/financeapp")
+    @GetMapping("/users")
     public List<User> getUsers() {
         return List.of(new User("user1", "password1"), new User("user2", "password2"));
     }
